6a701d1ebed75dff204484610334ea2346d14b74
Agis Anastasopoulos <corestudiosinc@gmail.com>
2012-08-22 23:20:19 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bathliving::Application.config.secret_token = '7084242364334e72ab9293d41156cb57ba0aaec18237adfb32320417186df95cd3ffdc25118e8eb7bf543fe003f50813226453c6751959cdfa9d522afbb61c2b'
