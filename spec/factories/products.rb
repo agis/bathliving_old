3924a9abd27840f4e659578392ebedd2b65a3f62
Agis Anastasopoulos <corestudiosinc@gmail.com>
2012-09-02 22:46:52 +0300

    Switch from #fixture_file_upload cause ActiveAdmin throws a 'stack level too deep' error when ActionDispatch::TestProccess is included. See https://github.com/gregbell/active_admin/issues/512

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,5 +1,3 @@
-include ActionDispatch::TestProcess
-
 FactoryGirl.define do
   factory :product do |f|
     f.name "A product name"
@@ -8,6 +6,6 @@ FactoryGirl.define do
   end
 
   factory :image do |f|
-    f.file { fixture_file_upload "spec/fixtures/img.jpg", "image/jpg" }
+    f.file { File.new("spec/fixtures/img.jpg") }
   end
 end
