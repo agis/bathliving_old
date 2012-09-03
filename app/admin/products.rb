cc02805fe1b75ca5535204c8dd86d0e19977db3c
Agis Anastasopoulos <corestudiosinc@gmail.com>
2012-09-03 04:29:44 +0300

    Add a simple label

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register Product do
   form multipart: true do |f|
-    f.inputs do
+    f.inputs "Product Information" do
       f.input :name
       f.input :description
     end
@@ -10,6 +10,7 @@ ActiveAdmin.register Product do
         p.input :file, hint: p.template.image_tag(p.object.file.url(:default))
       end
     end
+
     f.buttons
   end
 end
