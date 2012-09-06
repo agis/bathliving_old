ffeaa020599303a63d525ec4c1e580a4f731e28e
Agis Anastasopoulos <corestudiosinc@gmail.com>
2012-09-06 16:59:24 +0300

    Remove the default image

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,4 +1,3 @@
 Paperclip::Attachment.default_options[:storage] = :s3
 Paperclip::Attachment.default_options[:s3_credentials] = YAML.load_file("#{Rails.root}/config/s3.yml")
-Paperclip::Attachment.default_options[:default_url] = 'missing.png'
 Paperclip::Attachment.default_options[:s3_host_name] = 's3-eu-west-1.amazonaws.com'
