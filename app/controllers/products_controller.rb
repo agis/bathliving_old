9aba34c89678d995d7a0f0d952664bc4be79527e
Agis Anastasopoulos <corestudiosinc@gmail.com>
2012-09-11 04:14:13 +0300

    Implement controller method to set the active categories in the view (for filter output)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,23 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.limit(16)
-    @categories = ProductCategory.all
+    set_categories(@products)
   end
 
   def show
     @product = Product.find(params[:id])
   end
+
+  private
+
+  def set_categories(products)
+    @categories = []
+
+    products.each do |p|
+      unless @categories.include? p.category
+        @categories << p.category
+      end
+    end
+  end
+
 end
