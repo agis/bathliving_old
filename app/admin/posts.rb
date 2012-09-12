eaeb9aa884709d8f33c981959e46c44b2d435fba
Agis Anastasopoulos <corestudiosinc@gmail.com>
2012-09-13 02:12:25 +0300

    Remove CkEditor completely

--- app/admin/posts.rb
+++ app/admin/posts.rb
@@ -40,7 +40,6 @@ ActiveAdmin.register Post do
       f.input :title,        label: 'Τίτλος'
       f.input :publish_date, label: 'Ημερομηνία δημοσίευσης', as: :ui_date_picker
       f.input :body,         label: 'Κείμενο', hint: markdown_link
-      # f.input :body, as: :ckeditor, input_html: { height: 600 }
     end
 
     f.actions
