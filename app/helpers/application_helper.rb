0fffd9f9e01efc885d989503a7284d9cbcb0b5b8
Agis Anastasopoulos <corestudiosinc@gmail.com>
2012-09-14 14:05:34 +0300

    Truncate preview text on latest projects & posts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,11 @@ module ApplicationHelper
 
     raw markdown.render(text)
   end
+
+  def smart_truncate(s, opts = {})
+    opts = { words: 10 }.merge(opts)
+    a = s.split(/[ ]+/)
+    n = opts[:words]
+    a[0...n].join(' ') + (a.size > n ? '...' : '')
+  end
 end
